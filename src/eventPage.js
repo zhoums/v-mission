521b1f4aff8005f218caa340c9239910990d37ce
周木生 <zhoumusheng@zhoumushengdeMacBook-Pro.local>
2019-02-14 01:27:32 +0800

    find a test way

--- src/eventPage.js
+++ src/eventPage.js
@@ -28,12 +28,12 @@ chrome.webRequest.onBeforeSendHeaders.addListener(
     }
   }, {
     urls: ['https://*.taobao.com/*']
-  }, ["blocking", "requestHeaders","extraHeaders"]
+  }, ["blocking", "requestHeaders", "extraHeaders"]
 );
 let darenPageUrl = [];
 //VSC 第二次V任务功能前缀
 let VSCpage = 1;
-let VSCpagesize = 1;
+let VSCpagesize = 200;
 let VSCtotalpage = -1;
 let VSCtoken = 'KE923jddudk3FYjWedkHH';
 let VSCtab;
@@ -87,10 +87,13 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
       }
       getDarenId(VSCpage, VSCpagesize).then((data) => {
 
-        console.log('getid res',VSCpage,data);
+        console.log('getid res', VSCpage, data);
         data.result.list.forEach(async (item, key) => {
           let needTurnpage = (key == (data.result.list.length - 1));
-          console.log('needTurnpage',needTurnpage)
+          if (VSCpage == 8) {
+            console.log('needTurnpage88888', key, needTurnpage)
+          }
+          // console.log('needTurnpage', needTurnpage)
           // console.log('each',key,needTurnpage,key == data.result.list.length - 1)
           let daren_main = false,
             qry_fans = false,
@@ -118,19 +121,24 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
             darenId: item.darenId,
             statType: 3
           }
-console.log('needTurnpage 2',item.darenId)
-          let darenMain = await getDarenMain(item.darenId);
-          console.log('needTurnpage 3')
-          initParam.fansCount = darenMain.fansCount || ""
-          initParam.agencyName = darenMain.darenAgency && darenMain.darenAgency.agencyName ? darenMain.darenAgency.agencyName : ""
+          if (VSCpage == 8 && key == 19) {
+            console.log('8&&19=====', key, needTurnpage)
+          }
+          let darenMain = null;
+          darenMain = await getDarenMain(item.darenId);
+          if (VSCpage == 8 && key == 19) {
+            console.log('8&&19+++++', key, darenMain)
+          }
+          initParam.fansCount = darenMain.fansCount || 0
+          initParam.agencyName = darenMain.darenAgency && darenMain.darenAgency.agencyName ? darenMain.darenAgency.agencyName : 0
           initParam.agencyUrl = darenMain.darenAgency && darenMain.darenAgency.agencyID ? `https://v.taobao.com/v/home?spm=a21xh.11250901.0.0.6a6f6b6fpgjIgW&userId=${darenMain.darenAgency.agencyID}` : ""
-          initParam.scoreDarenCapacity = darenMain.darenScore || ""
+          initParam.scoreDarenCapacity = darenMain.darenScore || 0
           initParam.orderTakingRate = darenMain.darenMissionData && darenMain.darenMissionData.receiveRate ? darenMain.darenMissionData.receiveRate : ""
           initParam.orderTakingResponseTime = darenMain.darenMissionData && darenMain.darenMissionData.responseTime ? darenMain.darenMissionData.responseTime : ""
-          initParam.serviceTotalCustomer = darenMain.darenMissionData && darenMain.darenMissionData.cooperateSellerCount ? darenMain.darenMissionData.cooperateSellerCount : ""
+          initParam.serviceTotalCustomer = darenMain.darenMissionData && darenMain.darenMissionData.cooperateSellerCount ? darenMain.darenMissionData.cooperateSellerCount : 0
           initParam.serviceTotalQuantity = darenMain.darenMissionData && darenMain.darenMissionData.completeMission ? darenMain.darenMissionData.completeMission : ""
           initParam.serviceType = darenMain.darenMissionData && darenMain.darenMissionData.servType ? darenMain.darenMissionData.servType : ""
-          initParam.serviceDomain = darenMain.area || ""
+          initParam.serviceDomain = darenMain.area || 0
           initParam.introduction = ""
 
           if (darenMain.desc) {
@@ -148,9 +156,9 @@ console.log('needTurnpage 2',item.darenId)
           initParam.identityType = "" // todo : not founded this item
           initParam.serviceScore = darenMain.darenMissionData && darenMain.darenMissionData.avgScore ? darenMain.darenMissionData.avgScore : ""
           initParam.orderTakingFinishRate = darenMain.darenMissionData && darenMain.darenMissionData.completeRate ? darenMain.darenMissionData.completeRate : "";
-console.log('needTurnpage 4')
+
           let darenContent7 = await getDarenContent(item.darenId);
-          console.log('needTurnpage 5')
+
           initParam.contentPub7Days = darenContent7.result && darenContent7.result.publish ? darenContent7.result.publish : 0;
           initParam.contentBrowse7Days = darenContent7.result && darenContent7.result.text_pv ? darenContent7.result.text_pv : 0;
           initParam.contentGuide7Days = darenContent7.result && darenContent7.result.ipv ? darenContent7.result.ipv : 0;
@@ -163,7 +171,7 @@ console.log('needTurnpage 4')
           sevenDays.contentGuide = darenContent7.result && darenContent7.result.ipv ? darenContent7.result.ipv : 0;
           sevenDays.contentLiveBrowse = darenContent7.result && darenContent7.result.live_pv ? darenContent7.result.live_pv : 0;
           sevenDays.contentVideoBrowse = darenContent7.result && darenContent7.result.video_pv ? darenContent7.result.video_pv : 0;
-console.log('getDarenContent7 success')
+
           let darenContent30 = await getDarenContent(item.darenId, 30);
           initParam.contentPub30Days = darenContent30.result && darenContent30.result.publish ? darenContent30.result.publish : 0;
           initParam.contentBrowse30Days = darenContent30.result && darenContent30.result.text_pv ? darenContent30.result.text_pv : 0;
@@ -177,7 +185,7 @@ console.log('getDarenContent7 success')
           thirtyDays.contentGuide = darenContent30.result && darenContent30.result.ipv ? darenContent30.result.ipv : 0;
           thirtyDays.contentLiveBrowse = darenContent30.result && darenContent30.result.live_pv ? darenContent30.result.live_pv : 0;
           thirtyDays.contentVideoBrowse = darenContent30.result && darenContent30.result.video_pv ? darenContent30.result.video_pv : 0;
-console.log('getDarenContent30 success')
+
           let darenContent90 = await getDarenContent(item.darenId, 90);
           initParam.contentPub90Days = darenContent90.result && darenContent90.result.publish ? darenContent90.result.publish : 0;
           initParam.contentBrowse90Days = darenContent90.result && darenContent90.result.text_pv ? darenContent90.result.text_pv : 0;
@@ -192,14 +200,17 @@ console.log('getDarenContent30 success')
           ninetyDays.contentLiveBrowse = darenContent90.result && darenContent90.result.live_pv ? darenContent90.result.live_pv : 0;
           ninetyDays.contentVideoBrowse = darenContent90.result && darenContent90.result.video_pv ? darenContent90.result.video_pv : 0;
 
-console.log('getDarenContent90 success')
+
           let qryFans = await getQryFans(item.darenId);
           initFans = Object.assign({}, initFans, qryFans)
           console.log('postVmission begin')
-          await postVmission(initParam, initFans, sevenDays, thirtyDays, ninetyDays, needTurnpage)
+          if (VSCpage == 8) {
+            console.log('needTurnpage99999', initParam, initFans, sevenDays, thirtyDays, ninetyDays, needTurnpage, key)
+          }
+          await postVmission(initParam, initFans, sevenDays, thirtyDays, ninetyDays, needTurnpage, key)
         })
-      }).catch((data)=>{
-        console.log('getDarenId error',data)
+      }).catch((data) => {
+        console.log('getDarenId error', data)
       })
     })
 
@@ -431,7 +442,7 @@ let getDarenId = (page = 1, pageSize = 1000) => {
 }
 let getDarenMain = darenId => {
   return new Promise((resolve, reject) => {
-    try{
+    try {
       $.ajax({
         url: `https://v.taobao.com/micromission/daren/daren_main_portalv3.do?userId=${darenId}&_ksTS=1548232314754_17`,
         // async:false,
@@ -444,11 +455,11 @@ let getDarenMain = darenId => {
           }
         },
         error(data) {
-          console.log('llllll',data)
+          console.log('llllll', data)
           reject(data.msg)
         }
       })
-    }catch(err){
+    } catch (err) {
       console.log('errerrerrerr')
       reject(err)
     }
@@ -473,84 +484,85 @@ let getDarenContent = (darenId, day = 7) => {
     })
   })
 }
-let postVmission = (param, fasnObj, sevenDays, thirtyDays, ninetyDays, turnpage = false) => {
-  $.ajax({
-    url: `${config.willbeServer}/tb/v/syncVTaskDetail.wb`,
-    type: 'post',
-    async:false,
-    headers: {
-      token: VSCtoken,
-      'Content-Type': 'application/x-www-form-urlencoded'
-    },
-    data: param,
-    success(response) {
-      // console.log(response)
-    }
-  })
-  $.ajax({
-    url: `${config.willbeServer}/tb/v/syncVTaskFans.wb`,
-    type: 'post',
-    async:false,
-    headers: {
-      token: VSCtoken,
-      'Content-Type': 'application/json'
-    },
-    data: JSON.stringify(fasnObj),
-    success(response) {
-      // console.log(response)
-    }
-  })
-  $.ajax({
-    url: `${config.willbeServer}/tb/v/syncVTaskStat.wb`,
-    type: 'post',
-    async:false,
-    headers: {
-      token: VSCtoken,
-      'Content-Type': 'application/x-www-form-urlencoded'
-    },
-    data: sevenDays,
-    success(response) {
-      // console.log(response)
-    }
-  })
-  $.ajax({
-    url: `${config.willbeServer}/tb/v/syncVTaskStat.wb`,
-    type: 'post',
-    async:false,
-    headers: {
-      token: VSCtoken,
-      'Content-Type': 'application/x-www-form-urlencoded'
-    },
-    data: thirtyDays,
-    success(response) {
-      // console.log(response)
-    }
-  })
-  $.ajax({
-    url: `${config.willbeServer}/tb/v/syncVTaskStat.wb`,
-    type: 'post',
-    async:false,
-    headers: {
-      token: VSCtoken,
-      'Content-Type': 'application/x-www-form-urlencoded'
-    },
-    data: ninetyDays,
-    success(response) {
-      console.log('syncVTaskStat response',turnpage,VSCpage,VSCtotalpage)
-    },
-    error(){
-      console.log('skjdaldjlfkl')
-    }
-  })
+let postVmission = (param, fasnObj, sevenDays, thirtyDays, ninetyDays, turnpage = false, key) => {
+  // $.ajax({
+  //   url: `${config.willbeServer}/tb/v/syncVTaskDetail.wb`,
+  //   type: 'post',
+  //   async: false,
+  //   headers: {
+  //     token: VSCtoken,
+  //     'Content-Type': 'application/x-www-form-urlencoded'
+  //   },
+  //   data: param,
+  //   success(response) {
+  //     // console.log(response)
+  //   }
+  // })
+  // $.ajax({
+  //   url: `${config.willbeServer}/tb/v/syncVTaskFans.wb`,
+  //   type: 'post',
+  //   async: false,
+  //   headers: {
+  //     token: VSCtoken,
+  //     'Content-Type': 'application/json'
+  //   },
+  //   data: JSON.stringify(fasnObj),
+  //   success(response) {
+  //     // console.log(response)
+  //   }
+  // })
+  // $.ajax({
+  //   url: `${config.willbeServer}/tb/v/syncVTaskStat.wb`,
+  //   type: 'post',
+  //   async: false,
+  //   headers: {
+  //     token: VSCtoken,
+  //     'Content-Type': 'application/x-www-form-urlencoded'
+  //   },
+  //   data: sevenDays,
+  //   success(response) {
+  //     // console.log(response)
+  //   }
+  // })
+  // $.ajax({
+  //   url: `${config.willbeServer}/tb/v/syncVTaskStat.wb`,
+  //   type: 'post',
+  //   async: false,
+  //   headers: {
+  //     token: VSCtoken,
+  //     'Content-Type': 'application/x-www-form-urlencoded'
+  //   },
+  //   data: thirtyDays,
+  //   success(response) {
+  //     // console.log(response)
+  //   }
+  // })
+  // $.ajax({
+  //   url: `${config.willbeServer}/tb/v/syncVTaskStat.wb`,
+  //   type: 'post',
+  //   async: false,
+  //   headers: {
+  //     token: VSCtoken,
+  //     'Content-Type': 'application/x-www-form-urlencoded'
+  //   },
+  //   data: ninetyDays,
+  //   success(response) {
+  //     console.log('syncVTaskStat response', turnpage, VSCpage, VSCtotalpage)
+  //   },
+  //   error() {
+  //     console.log('skjdaldjlfkl')
+  //   }
+  // })
+  console.log('test ii', VSCpage, VSCtotalpage, turnpage, key)
   if (turnpage && VSCpage < VSCtotalpage) {
-    console.log('turn',turnpage,VSCpage,VSCtotalpage)
+    console.log('turn', turnpage, VSCpage, VSCtotalpage)
     VSCpage++;
     chrome.tabs.sendRequest(VSCtab, {
       greeting: "vm-turnpage",
-      page:VSCpage
+      page: VSCpage
     })
   } else if (turnpage && VSCpage == VSCtotalpage) {
-    console.log('turn ELSE',turnpage,VSCpage,VSCtotalpage)
+    console.log('turn ELSE', turnpage, VSCpage, VSCtotalpage)
     VSCpage = 1;
     VSCtotalpage = -1;
   }
